47fab54c5ce173ec8f426f7d48dd49559b120533
Dmytro <pase4nikdv@gmail.com>
2023-03-31 14:38:04 +0200

    added test table

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,30 +10,64 @@ const Home = () => {
         <Currency />
       </div>
       <div className="main">
-        <table className="table">
-          <tr>
-            <th className="cell">Date</th>
-            <th className="cell">Type</th>
-            <th className="cell">Category</th>
-            <th className="cell">Comment</th>
-            <th className="cell">Sum</th>
-            <th className="cell"></th>
-          </tr>
-          <tr>
-            <td className="cell">2022-01-01</td>
-            <td className="cell">Expense</td>
-            <td className="cell">Groceries</td>
-            <td className="cell">Bought food for week</td>
-            <td className="cell">$50.00</td>
-            <td className="cell actions">
-              <button href="#" className="edit">
-                I
-              </button>
-              <button href="#" className="button button--small">
-                Delete
-              </button>
-            </td>
-          </tr>
+        <table className="transactionsTable">
+          <thead>
+            <tr>
+              <th className="cell">Date</th>
+              <th className="cell">Type</th>
+              <th className="cell">Category</th>
+              <th className="cell">Comment</th>
+              <th className="cell textAlignL">Sum</th>
+              <th className="cell"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="cell">2022-01-01</td>
+              <td className="cell">+</td>
+              <td className="cell">Groceries</td>
+              <td className="cell">Bought food for week</td>
+              <td className="cell">5 000.00</td>
+              <td className="cell actions">
+                <button href="#" className="edit">
+                  I
+                </button>
+                <button href="#" className="button button--small">
+                  Delete
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="cell">2022-01-01</td>
+              <td className="cell">+</td>
+              <td className="cell">Groceries</td>
+              <td className="cell">Bought food for week</td>
+              <td className="cell">5 000.00</td>
+              <td className="cell actions">
+                <button href="#" className="edit">
+                  I
+                </button>
+                <button href="#" className="button button--small">
+                  Delete
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="cell">2022-01-01</td>
+              <td className="cell">+</td>
+              <td className="cell">Groceries</td>
+              <td className="cell">Bought food for week</td>
+              <td className="cell">5 000.00</td>
+              <td className="cell actions">
+                <button href="#" className="edit">
+                  I
+                </button>
+                <button href="#" className="button button--small">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
