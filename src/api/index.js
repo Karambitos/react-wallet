310a59a21e9f349b65fedd2786faf9fc9ef4ec5b
Dmytro <pase4nikdv@gmail.com>
2023-04-01 12:28:13 +0200

    added Chart component / added transactions Reduser

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = `https://connections-api.herokuapp.com/`;
+axios.defaults.baseURL = 'https://wallet.goit.ua';
 
 export const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
