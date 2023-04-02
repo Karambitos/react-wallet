5b168a10d34a25971f3f93612eb0cf0ebda0de0d
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-04-02 13:18:47 +0300

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { IconButton } from 'components/IconButton/IconButton';
 import HomeTab from 'components/HomeTab/HomeTab';
 import {TransactionsList} from 'components/TransactionsList/'
-// import { ReactComponent as EditIcon } from 'images/edit-pensil.svg';
 
 const Home = () => {
   return (
