aba9a67a4f9047b9633d7f4b8e951f558048250f
Inanim4te <Obsidianuz689@gmail.com>
2023-04-02 18:04:10 +0300

    Update Balance.jsx

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,10 +1,21 @@
 import { useSelector } from 'react-redux';
 import { getBalanse } from 'redux/auth/authSelectors';
 import style from './balance.module.scss';
+import { useEffect, useState } from 'react';
 
 const Balance = () => {
+  const [balanceAfter, setBalanceAfter] = useState(0);
   const userBalance = useSelector(getBalanse);
+  const transactions = useSelector(state => state.transactions.transactions);
   const formattedUserBalance = `₴ ${formattedValue(userBalance.toFixed(2))}`;
+  const formattedBalanceAfter = `₴ ${formattedValue(balanceAfter.toFixed(2))}`;
+
+  useEffect(() => {
+    if (transactions.length !== 0) {
+      console.log(transactions);
+      setBalanceAfter(transactions[transactions.length - 1].balanceAfter);
+    }
+  }, [transactions]);
 
   function formattedValue(value) {
     const formattedNum = Math.abs(value).toFixed(2);
@@ -19,7 +30,9 @@ const Balance = () => {
     <div className={style.balance}>
       <div className={style.balancewrapper}>
         <span className={style.balancetext}>Your balance</span>
-        <span className={style.balancesumm}>{formattedUserBalance}</span>
+        <span className={style.balancesumm}>
+          {formattedBalanceAfter || formattedUserBalance}
+        </span>
       </div>
     </div>
   );
