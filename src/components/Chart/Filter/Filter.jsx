6432d2710b890d1bfae3615c769da9408f7da2c8
Dmytro <pase4nikdv@gmail.com>
2023-04-02 20:10:02 +0200

    Selector bugfix

--- src/components/Chart/Filter/Filter.jsx
+++ src/components/Chart/Filter/Filter.jsx
@@ -116,45 +116,17 @@ export default function Filter() {
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <Selector
-        // defaultInputValue={'months'}
         options={years}
         onSelect={handleYearChange}
         styles={styles}
         placeholder={year.toString()}
       />
       <Selector
-        // defaultInputValue={month}
         options={months}
         onSelect={handleMonthChange}
         styles={styles}
-        placeholder={months[month].name}
+        placeholder={months[month - 1].name}
       />
-      {/* <select id="year" name="year" value={year} onChange={handleYearChange}>
-        <option value={2023}>2023</option>
-        <option value={2022}>2022</option>
-        <option value={2021}>2021</option>
-        <option value={2020}>2020</option>
-        <option value={2019}>2019</option>
-      </select>
-      <select
-        id="month"
-        name="month"
-        value={month}
-        onChange={handleMonthChange}
-      >
-        <option value={1}>January</option>
-        <option value={2}>February</option>
-        <option value={3}>March</option>
-        <option value={4}>April</option>
-        <option value={5}>May</option>
-        <option value={6}>June</option>
-        <option value={7}>July</option>
-        <option value={8}>August</option>
-        <option value={9}>September</option>
-        <option value={10}>October</option>
-        <option value={11}>November</option>
-        <option value={12}>December</option>
-      </select> */}
     </form>
   );
 }
