b29e4216bde39b10fd3a1d793d079cfe28f028e3
fdariia <fdariya3005@gmail.com>
2023-04-03 13:24:08 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import '../main.scss';
 import { PrivateRoute } from 'hoc/PrivateRoute';
 import { PublicRoute } from 'hoc/PublicRoute';
 import Layout from './Layout/Layout';
+import Loader from './Loader/Loader';
 
 const Home = lazy(() => import('pages/Home'));
 const Dashboard = lazy(() => import('pages/Dashboard'));
@@ -18,36 +19,42 @@ export default function App() {
   return (
     <>
       <ToastContainer position="top-center" autoClose={1000} />
-      <Routes>
-        <Route path="/" element={<Layout />}>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <PrivateRoute redirectTo="/login" component={<Home />} />
+              }
+            />
+            <Route
+              path="/Dashboard"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Dashboard />} />
+              }
+            />
+            <Route
+              path="/statistics"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Statistics />} />
+              }
+            />
+          </Route>
           <Route
-            index
-            element={<PrivateRoute redirectTo="/login" component={<Home />} />}
+            path="/login"
+            element={<PublicRoute redirectTo="/" component={<LoginPage />} />}
           />
           <Route
-            path="/Dashboard"
+            path="/register"
             element={
-              <PrivateRoute redirectTo="/login" component={<Dashboard />} />
+              <PublicRoute redirectTo="/" component={<RegisterPage />} />
             }
           />
-          <Route
-            path="/statistics"
-            element={
-              <PrivateRoute redirectTo="/login" component={<Statistics />} />
-            }
-          />
-        </Route>
-        <Route
-          path="/login"
-          element={<PublicRoute redirectTo="/" component={<LoginPage />} />}
-        />
-        <Route
-          path="/register"
-          element={<PublicRoute redirectTo="/" component={<RegisterPage />} />}
-        />
-        <Route path="/BaseStyle" element={<BaseStyle />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="/BaseStyle" element={<BaseStyle />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
