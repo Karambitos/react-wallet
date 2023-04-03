c319010817a874281b07bcd77c5368be5de5bd9f
Dmytro <pase4nikdv@gmail.com>
2023-04-03 09:27:55 +0200

    lazy update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,24 @@ import { ToastContainer } from 'react-toastify';
 import '../main.scss';
 import { PrivateRoute } from 'hoc/PrivateRoute';
 import { PublicRoute } from 'hoc/PublicRoute';
-import Layout from './Layout/Layout';
+// import Layout from './Layout/Layout';
 
-const Home = lazy(() => import('pages/Home'));
-const Dashboard = lazy(() => import('pages/Dashboard'));
-const Statistics = lazy(() => import('pages/Statistics'));
-const LoginPage = lazy(() => import('pages/LoginPage'));
-const RegisterPage = lazy(() => import('pages/RegisterPage'));
-const BaseStyle = lazy(() => import('pages/BaseStyle'));
-const NotFound = lazy(() => import('pages/NotFound'));
+import Layout from '../components/Layout/Layout';
+import Home from 'pages/Home';
+import Dashboard from 'pages/Dashboard';
+import Statistics from 'pages/Statistics';
+import NotFound from 'pages/NotFound';
+import BaseStyle from 'pages/BaseStyle';
+import { RegisterPage } from 'pages/RegisterPage';
+import { LoginPage } from 'pages/LoginPage';
+
+// const Home = lazy(() => import('pages/Home'));
+// const Dashboard = lazy(() => import('pages/Dashboard'));
+// const Statistics = lazy(() => import('pages/Statistics'));
+// const LoginPage = lazy(() => import('pages/LoginPage'));
+// const RegisterPage = lazy(() => import('pages/RegisterPage'));
+// const BaseStyle = lazy(() => import('pages/BaseStyle'));
+// const NotFound = lazy(() => import('pages/NotFound'));
 
 export default function App() {
   return (
