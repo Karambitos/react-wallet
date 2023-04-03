6a7d2c4a8d8895ba336101aba7937d49581b142d
Dmytro <pase4nikdv@gmail.com>
2023-04-03 09:34:00 +0200

    Dashboard remove

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import { PublicRoute } from 'hoc/PublicRoute';
 
 import Layout from '../components/Layout/Layout';
 import Home from 'pages/Home';
-import Dashboard from 'pages/Dashboard';
 import Statistics from 'pages/Statistics';
 import NotFound from 'pages/NotFound';
 import BaseStyle from 'pages/BaseStyle';
@@ -16,7 +15,6 @@ import { RegisterPage } from 'pages/RegisterPage';
 import { LoginPage } from 'pages/LoginPage';
 
 // const Home = lazy(() => import('pages/Home'));
-// const Dashboard = lazy(() => import('pages/Dashboard'));
 // const Statistics = lazy(() => import('pages/Statistics'));
 // const LoginPage = lazy(() => import('pages/LoginPage'));
 // const RegisterPage = lazy(() => import('pages/RegisterPage'));
@@ -32,12 +30,6 @@ export default function App() {
           <Route
             index
             element={<PrivateRoute redirectTo="/login" component={<Home />} />}
-          />
-          <Route
-            path="/Dashboard"
-            element={
-              <PrivateRoute redirectTo="/login" component={<Dashboard />} />
-            }
           />
           <Route
             path="/statistics"
