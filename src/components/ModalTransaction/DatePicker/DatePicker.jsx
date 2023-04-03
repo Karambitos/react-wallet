729cd0e950f1a775f7a80748fc36226e7a6be28c
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-04-03 14:00:31 +0300

    fixed positioning for date picker

--- src/components/ModalTransaction/DatePicker/DatePicker.jsx
+++ src/components/ModalTransaction/DatePicker/DatePicker.jsx
@@ -4,7 +4,7 @@ import css from './DatePicker.module.scss';
 import { ReactComponent as DatePickerIcon } from 'assets/imgages/DatePickerIcon.svg';
 
 export const DatePicker = ({ onSelect }) => {
- const handleChange = e => {
+  const handleChange = e => {
     onSelect(e.format('DD.MM.YYYY'));
   };
 
@@ -15,6 +15,7 @@ export const DatePicker = ({ onSelect }) => {
   };
   return (
     <Datetime
+      className={css.rdtPickerOpenUpwards}
       renderInput={renderInput}
       closeOnSelect
       onChange={handleChange}
@@ -22,4 +23,3 @@ export const DatePicker = ({ onSelect }) => {
     />
   );
 };
-
