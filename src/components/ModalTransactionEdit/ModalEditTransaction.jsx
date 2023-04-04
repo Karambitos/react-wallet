26a4c5a3df25b8bf90ec11ef0bb86e28a74b9345
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-04-05 00:13:06 +0300

    update
    slash added
    close button hover added

--- src/components/ModalTransactionEdit/ModalEditTransaction.jsx
+++ src/components/ModalTransactionEdit/ModalEditTransaction.jsx
@@ -7,9 +7,9 @@ import { useDispatch } from 'react-redux';
 import { fetchUpdateTransactions } from 'redux/transactions/operations';
 import { toast } from 'react-toastify';
 import { ReactComponent as CloseIcon } from '../../assets/imgages/close.svg';
-import Selector from '../Selector/Selector';
+
 import CustomSelect from './CustomSelect/CustomSelect';
-import styles from './ModalTransaction.module.scss';
+import styles from './ModalTransactionEdit.module.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
 const MAX_AMOUNT = 10000000;
@@ -47,34 +47,7 @@ export const ModalEditTransaction = ({ onClose, transaction }) => {
     }));
   };
 
-  const getCategoryText = categoryId => {
-    switch (categoryId) {
-      case 'c9d9e447-1b83-4238-8712-edc77b18b739':
-        return 'Main Expenses';
-      case '27eb4b75-9a42-4991-a802-4aefe21ac3ce':
-        return 'Products';
-      case 'bbdd58b8-e804-4ab9-bf4f-695da5ef64f4':
-        return 'Self care';
-      case '3caa7ba0-79c0-40b9-ae1f-de1af1f6e386':
-        return 'Car';
-      case '76cc875a-3b43-4eae-8fdb-f76633821a34':
-        return 'Child care';
-      case '128673b5-2f9a-46ae-a428-ec48cf1effa1':
-        return 'Household products';
-      case '1272fcc4-d59f-462d-ad33-a85a075e5581':
-        return 'Education';
-      case 'c143130f-7d1e-4011-90a4-54766d4e308e':
-        return 'Leisure';
-      case '719626f1-9d23-4e99-84f5-289024e437a8':
-        return 'Other expenses';
-      case '3acd0ecd-5295-4d54-8e7c-d3908f4d0402':
-        return 'Entertainment';
-      case '063f1132-ba5d-42b4-951d-44011ca46262':
-        return 'Income';
-      default:
-        return;
-    }
-  };
+
 
   const handleCategorySelect = categoryId => {
     setSelectedCategoryId(categoryId);
@@ -144,7 +117,7 @@ export const ModalEditTransaction = ({ onClose, transaction }) => {
       amount: amount,
     };
 
-    await dispatch(
+     dispatch(
       fetchUpdateTransactions({
         transactionId: transaction.id,
         credentials: updatedTransaction,
@@ -160,10 +133,9 @@ export const ModalEditTransaction = ({ onClose, transaction }) => {
         <button className={styles.closeButton} type="button" onClick={onClose}>
           <CloseIcon className={styles.closeButtonIcon} />
         </button>
-        {/* {children} */}
-
+     
+       
         <h1 className={styles.title}>Edit transaction</h1>
-
         <div className={styles.toggleContainer}>
           <span
             onClick={toggle}
@@ -173,7 +145,7 @@ export const ModalEditTransaction = ({ onClose, transaction }) => {
           >
             Income
           </span>
-
+<span className={styles.toggleText}>/</span>
           <span
             onClick={toggle}
             className={`${styles.toggleText} ${
